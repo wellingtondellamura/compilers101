d1b2bd2f76d305197ac8272e498c9675ec3b6c67
wellington <wellington@dijkstra13>
2022-06-13 17:02:13 -0300

    lexer

---
+++ first-steps/very-simple-interpreter/Lexer/Token.cs
@@ -0,0 +1,15 @@
+namespace very_simple_interpreter.Lexer
+{
+    public class Token
+    {
+
+        public Token(ETokenType type, Int32? value = null)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public ETokenType Type { get; set; }
+        public Int32? Value{ get; set; }
+    }
+}
